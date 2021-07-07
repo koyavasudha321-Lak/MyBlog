61b2b469397901168bed5d1de634adfead790077
koyavasudha321-Lak <71616728+koyavasudha321-Lak@users.noreply.github.com>
2021-07-08 00:15:02 +0530

    Login,Signup, Navigation, logout done!!!!

---
+++ DJANGO/MyBlog/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import register,login_view,logout_view
+
+urlpatterns =[
+	path('signup/',register,name='signup'),
+	path('login/',login_view,name='login'),
+	path('logout/',logout_view,name='logout'),
+
+]
